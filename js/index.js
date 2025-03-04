994aa18f528208d63d9d65077b3157ae88e5a335
LilianaAmangoua <amangoua99@gmail.com>
2025-03-04 20:04:07 +0100

    Added CSS to style filters and started main functionnality (check and uncheck a task)

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 
+// Gestion de la sidebar
 function toggleMenu() {
     const menu = document.querySelector(".menu");
     menu.classList.toggle("active");
 }
 
 const containerTasks = document.querySelector(".container-tasks");
-const onGoingTasks = document.querySelector(".container-ongoingTask");
-const finishedTasks = document.querySelector(".container-finished");
 
-
+// Créer une carte tâche
 function createTask(task, container) {
+
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+
+    const label = document.createElement("label");
+    const taskCheck = document.createElement("input");
+    taskCheck.type = "checkbox";
+    taskCheck.id = "task-check";
+    label.appendChild(taskCheck);
+
+
 
     const taskCard = document.createElement("div");
     taskCard.classList.add("task-card");
@@ -25,45 +35,65 @@ function createTask(task, container) {
 
     const taskPriority = document.createElement("p");
     taskPriority.innerText = task.priority;
+    if(taskPriority.innerText === "Basse") {
+        taskPriority.classList.add("priority-card-low");
+    } else if(taskPriority.innerText === "Moyenne") {
+        taskPriority.classList.add("priority-card-medium");
+    } else if(taskPriority.innerText === "Haute") {
+        taskPriority.classList.add("priority-card-high");
+    } else {
+        taskPriority.classList.add("priority-card-none");
+    }
 
     const taskOptions = document.createElement("i");
     taskOptions.innerHTML = "<i class=\"fa-solid fa-ellipsis-vertical\"></i>";
 
-    taskCard.appendChild(taskText);
+    taskCard.appendChild(taskContainer);
+    taskContainer.appendChild(label);
+    taskContainer.appendChild(taskText);
+    taskContainer.appendChild(taskOptions);
     taskCard.appendChild(taskDate);
     taskCard.appendChild(taskDescription);
     taskCard.appendChild(taskPriority);
-    taskCard.appendChild(taskOptions);
+
 
     container.appendChild(taskCard);
+
+    taskCheck.addEventListener("change", () => {
+        if (taskCheck.checked) {
+            task.status = "terminées";
+        } else {
+            task.status = "en cours";
+        }
+
+    })
+
 }
 
+// Au rechargement, récupérer les tâches existantes
 const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
 document.addEventListener("DOMContentLoaded", () => {
     taskList.forEach(task => {
-        if (task.status.toLowerCase() === "en cours") {
-            createTask(task, onGoingTasks);
-        } else if (task.status.toLowerCase() === "terminé") {
-            createTask(task, finishedTasks);
-        } else {
             createTask(task, containerTasks);
-        }
     })
 });
 
+
+// Afficher les tâches en fonction de leur statut (en cours, terminées)
 const filterItems = document.querySelectorAll(".filter-item");
 
 
 filterItems.forEach((filterItem) => {
     filterItem.addEventListener("click", function (event) {
+        filterItems.forEach((filterItem) => {
+            filterItem.classList.remove("active");
+        })
         event.preventDefault();
-        console.log("Filter item is clicked")
+        //console.log("Filter item is clicked")
         filterItem.classList.toggle("active");
         const tasks = JSON.parse(localStorage.getItem("taskList")) || [];
 
         containerTasks.innerHTML = "";
-        onGoingTasks.innerHTML = "";
-        finishedTasks.innerHTML = "";
 
         if (tasks.length > 0) {
             if (filterItem.innerText.toLowerCase() === "en cours") {
@@ -73,7 +103,7 @@ filterItems.forEach((filterItem) => {
                     }
                 })
                 filteredTasks.forEach(task => {
-                    createTask(task, onGoingTasks)
+                    createTask(task, containerTasks)
                 })
             } else if (filterItem.innerText.toLowerCase() === "terminées") {
                 const filteredTasks = tasks.filter((item) => {
@@ -82,7 +112,7 @@ filterItems.forEach((filterItem) => {
                     }
                 })
                 filteredTasks.forEach(task => {
-                    createTask(task, finishedTasks)
+                    createTask(task, containerTasks)
                 })
             } else {
                 tasks.forEach(task => {
@@ -95,6 +125,7 @@ filterItems.forEach((filterItem) => {
 })
 
 
+// Gestion de l'ouverture du dialog (modale)
 const dialog = document.querySelector("#addNewTask");
 const closeDialog = document.querySelector(".close-dialog");
 const addNewTaskForm = document.querySelector("#addNewTaskForm");
@@ -110,6 +141,7 @@ function closeModal() {
 
 closeDialog.addEventListener("click", closeModal);
 
+// Ajouter une nouvelle tâche
 addNewTaskForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -135,9 +167,11 @@ addNewTaskForm.addEventListener("submit", function (event) {
     closeModal();
 });
 
+// Deplacer une tâche dans "terminées"
+
+
 
 // Barre de recherche
-
 const searchBar = document.querySelector("#search");
 searchBar.addEventListener("input", function (event) {
     event.preventDefault();
@@ -150,7 +184,6 @@ searchBar.addEventListener("input", function (event) {
            createTask(task, containerTasks);
         }
     })
-
 })
 
 
